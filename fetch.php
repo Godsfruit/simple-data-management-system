3c11fc8a0f6e3506b5ba3303733f05c71ece001f
Godsfruit <godsfruitbookings@gmail.com>
2026-03-20 12:39:37 +0100

    fetch.php

---
+++ fetch.php
@@ -0,0 +1,24 @@
+<?php
+$result = $conn->query("SELECT * FROM records");
+
+echo "<table border='1'>
+<tr>
+<th>ID</th>
+<th>Name</th>
+<th>Location</th>
+<th>Data Value</th>
+<th>Date</th>
+</tr>";
+
+while($row = $result->fetch_assoc()) {
+    echo "<tr>
+    <td>{$row['id']}</td>
+    <td>{$row['name']}</td>
+    <td>{$row['location']}</td>
+    <td>{$row['data_value']}</td>
+    <td>{$row['date_created']}</td>
+    </tr>";
+}
+
+echo "</table>";
+?>
